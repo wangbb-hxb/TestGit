a69e59705475fe8bd5b1f63fe6818c1a79c07f2b
wangbingbing <wangbingbing@163.com>
2020-12-18 11:04:32 +0800

    test

--- src/main/java/com/alibing/git/Apple.java
+++ src/main/java/com/alibing/git/Apple.java
@@ -1,5 +1,20 @@
 package com.alibing.git;
 
 public class Apple {
+	
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
