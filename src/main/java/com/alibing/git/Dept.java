6785fda41a7826373a549b3a69810a662ba3a389
wangbingbing <wangbingbing@163.com>
2020-12-18 13:23:07 +0800

    this is a dept dev

---
+++ src/main/java/com/alibing/git/Dept.java
@@ -0,0 +1,16 @@
+package com.alibing.git;
+
+public class Dept {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
